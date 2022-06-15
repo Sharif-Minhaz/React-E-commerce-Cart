6bc9de7d59edcc06ce0adb10653b01f780e2a2f4
Sharif Md. Minhazur Rahman Rabbi <96350875+Sharif-Minhaz@users.noreply.github.com>
2022-06-15 18:49:24 +0600

    feat: new logo and favicon icon added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,9 +4,7 @@ function Navbar(props) {
 	return (
 		<nav className="navbar navbar-expand navbar-light bg-light sticky-top">
 			<div className="container justify-content-between">
-				<a className="navbar-brand" href="#">
-					Navbar
-				</a>
+				<img src="/images/logo.png" width="145px" alt="logo" />
 				<div className="">
 					<ul className="navbar-nav me-auto mb-0">
 						<li className="nav-item">
